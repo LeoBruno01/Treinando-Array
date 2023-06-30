cffa38a9b37b7f6403e67c28606af9804f029696
Leonardo Bruno <121522941+LeoBruno01@users.noreply.github.com>
2023-06-30 15:26:56 -0300

    Array com For e ForEach

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("***** DIGITE 5 NOMES E FAÇA A ESCOLHA DE UM EXISTENTE *****");
+void EscolheNome()
+{
+    string[] nomes = new string[5];
+
+    for (int i = 0; i < nomes.Length; i++)
+    {
+        Console.Write($"Escolha o {i + 1}º nome: ");
+        nomes[i] = Console.ReadLine();
+    }
+
+    Console.Write("Qual nome você deseja escolher? ");
+    var busca = Console.ReadLine();
+
+    foreach (string palavra in nomes)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"Parabéns,o nome {busca} existe no seu banco de dados.");
+            return;
+        }
+    }
+}
+
+EscolheNome();
